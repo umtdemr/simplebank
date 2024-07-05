5cf0e0c737a4fa016b6a54a6a545351a2dfb266e
umtdemr <umitde692@gmail.com>
2024-07-05 21:26:20 +0300

    use pgx pool for concurrency & create/test transaction

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -2,24 +2,26 @@ package db
 
 import (
 	"context"
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"log"
 	"os"
 	"testing"
 )
 
 var testQueries *Queries
+var testDb *pgxpool.Pool
 
 const dbURL = "postgresql://simple_bank:simple_bank@localhost:5432/simple_bank?sslmode=disable"
 
 func TestMain(m *testing.M) {
-	conn, err := pgx.Connect(context.Background(), dbURL)
+	var err error
+	testDb, err = pgxpool.New(context.Background(), dbURL)
 
 	if err != nil {
 		log.Fatal("cannot connect to db", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDb)
 
 	os.Exit(m.Run())
 }
---
+++ db/sqlc/store.go
@@ -0,0 +1,89 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Store struct {
+	*Queries
+	db *pgxpool.Pool
+}
+
+func NewStore(db *pgxpool.Pool) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+func (s *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := s.db.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		rbErr := tx.Rollback(ctx)
+		if rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+	return tx.Commit(ctx)
+}
+
+type TransferTxParams struct {
+	FromAccountId pgtype.Int8 `json:"from_account_id"`
+	ToAccountId   pgtype.Int8 `json:"to_account_id"`
+	Amount        int64       `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+func (s *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+	err := s.execTx(ctx, func(q *Queries) error {
+		var err error
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountId,
+			ToAccountID:   arg.ToAccountId,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountId,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountId,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		// TODO :update accounts balance
+
+		return nil
+	})
+	return result, err
+}
