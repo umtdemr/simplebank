f6af17e296d10858c2fcfe69ce8e6925bad4aeba
umtdemr <umitde692@gmail.com>
2024-07-24 18:17:03 +0300

    extract grpc metadata

---
+++ gapi/metadata.go
@@ -0,0 +1,41 @@
+package gapi
+
+import (
+	"context"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
+)
+
+const (
+	grpcGatewayUserAgentHeader = "grpcgateway-user-agent"
+	userAgentHeader            = "user-agent"
+	xForwardedForHeader        = "x-forwarded-host"
+)
+
+type Metadata struct {
+	UserAgent string
+	ClientIP  string
+}
+
+func (server *Server) extractMetadata(ctx context.Context) *Metadata {
+	mtdt := &Metadata{}
+
+	if md, ok := metadata.FromIncomingContext(ctx); ok {
+		if userAgents := md.Get(grpcGatewayUserAgentHeader); len(userAgents) > 0 {
+			mtdt.UserAgent = userAgents[0]
+		}
+		if userAgents := md.Get(userAgentHeader); len(userAgents) > 0 {
+			mtdt.UserAgent = userAgents[0]
+		}
+
+		if clientIps := md.Get(xForwardedForHeader); len(clientIps) > 0 {
+			mtdt.ClientIP = clientIps[0]
+		}
+
+	}
+
+	if p, ok := peer.FromContext(ctx); ok {
+		mtdt.ClientIP = p.Addr.String()
+	}
+	return mtdt
+}
--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -39,12 +39,15 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 
 	refreshPayloadId := pgtype.UUID{Valid: true, Bytes: refreshPayload.ID}
 	refreshPayloadExpiresAt := pgtype.Timestamptz{Valid: true, Time: refreshPayload.ExpiredAt}
+
+	mtdt := server.extractMetadata(ctx)
+
 	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
 		ID:           refreshPayloadId,
 		Username:     user.Username,
 		RefreshToken: refreshToken,
-		UserAgent:    "",
-		ClientIp:     "",
+		UserAgent:    mtdt.UserAgent,
+		ClientIp:     mtdt.ClientIP,
 		IsBlocked:    false,
 		ExpiresAt:    refreshPayloadExpiresAt,
 	})
