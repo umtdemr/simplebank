0101a0786204612af1243703fd3a787e630ee151
umtdemr <umitde692@gmail.com>
2024-07-29 14:02:14 +0300

    add http logger

--- main.go
+++ main.go
@@ -129,7 +129,10 @@ func runGatewayServer(config util.Config, store db.Store) {
 	mux.Handle("/swagger/", swaggerHandler)
 
 	log.Info().Msgf("start HTTP gateway server at %s", listener.Addr().String())
-	err = http.Serve(listener, mux)
+
+	handler := gapi.HttpLogger(mux)
+
+	err = http.Serve(listener, handler)
 	if err != nil {
 		log.Fatal().Msg("cannot start gRPC server")
 	}
