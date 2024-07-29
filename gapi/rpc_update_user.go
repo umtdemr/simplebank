576fbb1e7ed039867f66b479a1a3abfdcf8b3480
umtdemr <umitde692@gmail.com>
2024-07-29 13:16:18 +0300

    add authorization for update user

--- gapi/rpc_update_user.go
+++ gapi/rpc_update_user.go
@@ -17,10 +17,18 @@ import (
 )
 
 func (server *Server) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
-	// TODO: ADD authorization
+	authPayload, err := server.authorizeUser(ctx)
+	if err != nil {
+		return nil, unauthenticatedError(err)
+	}
+
 	violations := validateUpdateUserRequest(req)
 	if len(violations) > 0 {
 		return nil, invalidArgumentError(violations)
+	}
+
+	if authPayload.Username != req.GetUsername() {
+		return nil, status.Errorf(codes.PermissionDenied, "cannot update other user's info")
 	}
 
 	arg := db.UpdateUserParams{
